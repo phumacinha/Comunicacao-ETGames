8205fbce9257498622418d042c24d857a0ce9d5f
Pedro <pedro.souza2@estudante.ufla.br>
2021-01-04 19:17:27 -0300

    refactor

---
+++ src/comunicacao/jogos/TipoDeJogo.java
@@ -0,0 +1,10 @@
+package comunicacao.jogos;
+
+/**Enum com os jogos disponíveis no servidor.
+ *
+ * @author Pedro Antônio de Souza
+ */
+public enum TipoDeJogo {
+    JOGO_DA_VELHA;
+
+}
--- src/comunicacao/mensagens/MensagemSocket.java
+++ src/comunicacao/mensagens/MensagemSocket.java
@@ -1,4 +1,4 @@
-package mensagemsocket;
+package comunicacao.mensagens;
 
 import java.io.Serializable;
 import java.util.HashMap;
